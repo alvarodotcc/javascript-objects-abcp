4fe94b3afd767fdc6375d00e5d9bb6f3db2b91a5
Alvaro Caballero <flatiron@alvaro.cc>
2018-01-15 22:34:25 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,11 +1,11 @@
 const playlist = { artistName: "songTitle"}
 
 function updatePlaylist (obj, key, value){
-  obj[key] = value
-  return obj
+  obj[key] = value;
+  return obj;
 }
 
 function removeFromPlaylist(obj, key){
-  delete obj.key;
+  delete obj[key];
   return obj;
 }
